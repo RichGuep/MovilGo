520061c4190a5dfb74236ab741d709d20af71c72
RichGuep <richard.guevara@greenmovil.com.co>
2026-05-08 14:43:15 -0500

    Update logic_programador.py

--- logic_programador.py
+++ logic_programador.py
@@ -1,24 +1,17 @@
 import streamlit as st
 
-
 def pantalla_tecnicos():
     st.title("👷 Programador Técnicos")
     st.success("Módulo técnicos funcionando")
-
 
 def pantalla_abordaje():
     st.title("🚍 Programador Personal Abordaje")
     st.success("Módulo abordaje funcionando")
 
-
 def pantalla_programador():
-
     modulo = st.radio(
         "Selecciona módulo",
-        [
-            "👷 Técnicos",
-            "🚍 Personal Abordaje"
-        ],
+        ["👷 Técnicos", "🚍 Personal Abordaje"],
         horizontal=True
     )
 
