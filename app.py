d1c67fe6ef2aa809d6ec5b85e78728f2317bebd8
RichGuep <richard.guevara@greenmovil.com.co>
2026-05-08 16:18:26 -0500

    Update app.py

--- app.py
+++ app.py
@@ -176,13 +176,21 @@ else:
             st.session_state.empresa = None
             st.rerun()
 
- if menu == "🏠 Inicio":
-    modulo_inicio()
-elif menu == "📅 Programación":
-    pantalla_programador()
-elif menu == "📋 Reporte Detallado":
-    modulo_detallado()
-elif menu == "👥 Personal":
-    modulo_personal()
-elif menu == "🧩 Grupos":
-    pantalla_asignacion_grupos()
+ # ============================
+    # ROUTER LIMPIO (IMPORTANTE)
+    # ============================
+
+    if menu == "🏠 Inicio":
+        modulo_inicio()
+
+    elif menu == "📅 Programación":
+        pantalla_programador()
+
+    elif menu == "📋 Reporte Detallado":
+        modulo_detallado()
+
+    elif menu == "👥 Personal":
+        modulo_personal()
+
+    elif menu == "🧩 Grupos":
+        pantalla_asignacion_grupos()
