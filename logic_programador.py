cce9c67368748c0c4d297dbed13d9546068b748f
RichGuep <richard.guevara@greenmovil.com.co>
2026-05-11 22:05:56 -0500

    Update logic_programador.py

--- logic_programador.py
+++ logic_programador.py
@@ -337,22 +337,4 @@ def pantalla_parametrizador():
     st.header("⚙️ Parametrizador")
 
 
-    def main():
-
-    import streamlit as st
-
-    menu = st.radio(
-        "Módulos",
-        ["Personal Técnico", "Personal de Abordaje", "Parametrizador"],
-        horizontal=True
-    )
-
-    if menu == "Personal Técnico":
-        pantalla_tecnico()
-
-    elif menu == "Personal de Abordaje":
-        pantalla_abordaje()
-
-    elif menu == "Parametrizador":
-        pantalla_parametrizador()
-    st.info("Aquí conectarás empleados.xlsx y asignación de grupos")
+  
